2f841deecd74645b3ff6f119df5bd8c3baef72e6
Pradeep Raj Thapaliya <pradeep.thapaliya95@gmail.com>
2024-03-11 17:35:46 -0400

    remove prt-ts package from core package

--- packages/spfx-core/src/index.ts
+++ packages/spfx-core/src/index.ts
@@ -1,14 +1,1 @@
 export * from './lib/spfx-core';
-
-export * from "@prt-ts/debounce"; 
-export * from "@prt-ts/fluent-common-features";
-export * from "@prt-ts/fluent-react-hook-form";
-export * from "@prt-ts/fluent-react-table-v2";
-export * from "@prt-ts/fluent-theme";
-
-export {
-    Switch as SwitchControl, 
-    Case,
-    For,
-    Show
-} from "@prt-ts/react-control-flow"
