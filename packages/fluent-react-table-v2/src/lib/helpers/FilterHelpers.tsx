c6d1ede6badb6e917dd41ae2916ef6e99b9ba5a0
Pradeep Raj Thapaliya <pradeep.thapaliya95@gmail.com>
2024-03-12 15:28:43 -0400

    null check

--- packages/fluent-react-table-v2/src/lib/helpers/FilterHelpers.tsx
+++ packages/fluent-react-table-v2/src/lib/helpers/FilterHelpers.tsx
@@ -3,7 +3,7 @@ import { FilterFn } from "@tanstack/react-table";
 export const arrIncludesSome: FilterFn<unknown> = (row, columnId, value) => {
     // Rank the item
     const rowValue = row.getValue(columnId);
-    const lowerCaseValues = value.map((v: string) => `${v || ''}`.toLowerCase());
+    const lowerCaseValues = (value || []).map((v: string) => `${v || ''}`.toLowerCase());
     const passed =
       Array.isArray(value) &&
       (value?.length === 0 || lowerCaseValues.includes(`${rowValue}`?.toLowerCase()));
