4659b44d422c95fa2ac2b7d4a5c2cb6559957e5c
Pradeep Raj Thapaliya <pradeep.thapaliya95@gmail.com>
2024-03-22 21:49:34 -0400

    common icons

---
+++ packages/fluent-common-features/src/lib/icons/index.tsx
@@ -0,0 +1,16 @@
+import {
+    AddFilled, AddRegular,
+    ArrowDownloadFilled,
+    ArrowDownloadRegular,
+    DeleteFilled, DeleteRegular,
+    EditFilled, EditRegular,
+    EyeFilled, EyeRegular,
+    bundleIcon
+} from "@fluentui/react-icons"; 
+
+export const AddIcon = bundleIcon(AddFilled, AddRegular);
+export const EditIcon = bundleIcon(EditFilled, EditRegular);
+export const ViewIcon = bundleIcon(EyeFilled, EyeRegular);
+export const DeleteIcon = bundleIcon(DeleteFilled, DeleteRegular);
+export const DownloadIcon = bundleIcon(ArrowDownloadFilled, ArrowDownloadRegular);
+
--- packages/fluent-common-features/src/lib/index.tsx
+++ packages/fluent-common-features/src/lib/index.tsx
@@ -36,3 +36,5 @@ export {
 export {
   useButtonStyles
 } from "./styles";
+
+export * from "./icons";
