2e3a0c326a6f26bd5526b1f5117ac8ae736d01fa
Pradeep Raj Thapaliya <pradeep.thapaliya95@gmail.com>
2024-04-04 15:40:56 -0400

    build issue fixed

--- scripts/build.bat
+++ scripts/build.bat
@@ -3,6 +3,9 @@
 REM # Build the debounce library
 call npx nx run debounce:build
 
+REM # Build the types library
+call npx nx run types:build
+
 REM # Build the export-helper library
 call npx nx run export-helpers:build
 
@@ -11,6 +14,9 @@ call npx nx run fluent-common-features:build
 
 REM # Build the fluent-formik library
 call npx nx run fluent-formik:build
+
+REM # Build the fluent-input-extensions library
+call npx nx run fluent-input-extensions:build
 
 REM # Build the fluent-react-hook-form library
 call npx nx run fluent-react-hook-form:build
