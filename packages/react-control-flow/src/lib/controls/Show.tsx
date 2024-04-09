733b652265a1285617813814bf7039a29412f33f
Pradeep Raj Thapaliya <pradeep.thapaliya95@gmail.com>
2024-04-09 06:53:56 -0400

    applying package update

--- packages/react-control-flow/src/lib/controls/Show.tsx
+++ packages/react-control-flow/src/lib/controls/Show.tsx
@@ -1,15 +1,16 @@
+import { ReactNode } from "react";
 import { ConditionClause } from "../types/ConditionalClause";
 
 interface ShowProps {
     when: ConditionClause;
-    children: JSX.Element | JSX.Element[] | null;
+    children: JSX.Element | JSX.Element[] | ReactNode | string | number | null;
     fallback?: JSX.Element | null;
 }
 /* eslint-disable */
-export const Show = (props: ShowProps): JSX.Element | JSX.Element[] | null => {
+export const Show = (props: ShowProps): JSX.Element | null => {
     const { when = false, children = null, fallback = null } = props;
     if (!when) {
-        return fallback as JSX.Element | null;
+        return <>{fallback}</>;
     }
-    return children as JSX.Element | JSX.Element[] | null;
+    return <>{children}</>;
 };
