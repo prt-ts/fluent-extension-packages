333caca84530cadec38dfc7be99619a15bf6eeea
Pradeep Raj Thapaliya <pradeep.thapaliya95@gmail.com>
2024-04-11 18:38:27 -0400

    nx latest, some build issue fixed

--- packages/fluent-react-hook-form/src/lib/components/types/CommonFieldProps.ts
+++ packages/fluent-react-hook-form/src/lib/components/types/CommonFieldProps.ts
@@ -1,7 +1,7 @@
 import { FieldProps, InfoLabelProps } from "@fluentui/react-components";
 
 
-export type CommonFieldProps = Omit<Pick<FieldProps, 'required' | 'hint' | 'orientation'>, "children">;
+export type CommonFieldProps = Pick<FieldProps, 'required' | 'hint' | 'orientation'>;
 export type CommonInfoLabelProps = Pick<InfoLabelProps, 'label' | 'required' | "info">;
 
 export type CommonFieldInfoLabelProps = CommonFieldProps & CommonInfoLabelProps & {
