f295bd4e49e083e9fdc1c8f403efe9a81009eeb4
Pradeep Raj Thapaliya <pradeep.thapaliya95@gmail.com>
2024-04-14 13:56:00 -0400

    grid loading page enhancements

--- packages/fluent-react-table-v2/src/lib/components/loading/Loading.tsx
+++ packages/fluent-react-table-v2/src/lib/components/loading/Loading.tsx
@@ -1,32 +1,45 @@
 import {
     Skeleton,
     SkeletonItem,
-} from "@fluentui/react-components";
-import * as React from "react";
+} from "@fluentui/react-components"; 
+import { Column, RowData, Table } from "@tanstack/react-table";
+import { For, Show } from "@prt-ts/react-control-flow";
 import { useLoadingStyles } from "./useLoadingStyles";
 
-type LoadingProps = {
+type LoadingProps<TItem extends RowData> = {
     numberOfItems?: number,
-    numberOfColumns?: number,
+    visibleColumns: Column<TItem, unknown>[],
+    table: Table<TItem>
 }
 
-export const Loading: React.FC<LoadingProps> = (props) => {
-    const { numberOfItems = 16, numberOfColumns = 5 } = props;
+export const Loading = <TItem extends RowData>(props: LoadingProps<TItem>) => {
+    const { numberOfItems = 16, visibleColumns, table } = props;
+    const selectionMode = table.options.meta?.rowSelectionMode || "none";
     const styles = useLoadingStyles();
-    return (<div className={styles.invertedWrapper}>
-        <Skeleton>
-            {
-                [...Array(numberOfItems)].map((_, i) => (<div key={i} className={styles.row}>
-                    {
-                        [...Array(numberOfColumns)].map((_, i) => {
-                            if (i === 0) {
-                                return <SkeletonItem key={i} shape="circle" size={24} />
-                            }
-                            return <SkeletonItem key={i} size={16} />
-                        })
-                    }
-                </div>))
-            }
-        </Skeleton>
-    </div>)
+    return (
+        <div style={{ width: "100%" }}>
+            <For each={[...Array(numberOfItems)]}>
+                {(rowIndex, index) => {
+                    return (
+                        <Skeleton key={`${rowIndex}_${index}`} className={styles.row} style={{ width: "100%" }}>
+                            <Show when={["single", "multiple"].includes(selectionMode)}>
+                                <SkeletonItem key={`${index}_select_row`} shape="circle" size={24} style={{ width: "40px" }}/>
+                            </Show>
+                            <For each={visibleColumns || []}>
+                                {(column, i) => (
+                                    <SkeletonItem
+                                        key={i}
+                                        style={{
+                                            minWidth: column.getSize(),
+                                            width: "100%"
+                                        }}
+                                    />
+                                )}
+                            </For>
+                        </Skeleton>
+                    )
+                }}
+            </For>
+        </div>
+    )
 }
