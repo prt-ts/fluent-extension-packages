0ea77d1294deb1d76248155caca1bbf92f1081a6
Pradeep Thapaliya <pradeep.thapaliya95@gmail.com>
2024-04-28 19:43:31 -0400

    theme color scheme

--- ui/examples/src/main.tsx
+++ ui/examples/src/main.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom/client';
 import * as React from 'react';
 
 import App from './app/app';
-import { FluentProvider, teamsLightTheme } from '@fluentui/react-components';
+import { FluentProvider, Spinner } from '@fluentui/react-components';
 import { ThemeService } from '@prt-ts/fluent-theme';
 import { AppFeatureProvider } from '@prt-ts/fluent-common-features';
 
@@ -13,19 +13,30 @@ const root = ReactDOM.createRoot(
 const { getTheme } = ThemeService();
 
 export const AppRoot: React.FC = () => {
-  const [mode, setMode] = React.useState<'light' | 'dark'>('light'); // ['light', 'dark'
-  const [theme, setTheme] = React.useState(teamsLightTheme);
+  const [mode, setMode] = React.useState<'light' | 'dark'>(() => {
+    const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    return isDarkMode.matches ? 'dark' : 'light';
+  });
+  const [theme, setTheme] = React.useState(null);
 
   React.useEffect(() => {
-    getTheme('#751d1f', mode === "dark", 0.8).then(setTheme).catch(console.error);
+    getTheme('#751d1f', mode === 'dark', 0.8)
+      .then(setTheme)
+      .catch(console.error);
   }, [mode]);
+
+  if (!theme) return <Spinner title="Please wait" />;
 
   return (
     <FluentProvider theme={theme}>
       <AppFeatureProvider>
-        <button onClick={() => {
-          setMode(mode === "light" ? "dark" : "light");
-        }}>{mode}</button>
+        <button
+          onClick={() => {
+            setMode(mode === 'light' ? 'dark' : 'light');
+          }}
+        >
+          {mode}
+        </button>
         <App />
       </AppFeatureProvider>
     </FluentProvider>
