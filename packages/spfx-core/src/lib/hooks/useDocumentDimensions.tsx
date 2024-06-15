1d0cae66a306156f290bb8392e1b15f4b693b193
Pradeep Thapaliya <pradeep.thapaliya95@gmail.com>
2024-06-15 17:51:03 -0400

    add helpers

--- packages/spfx-core/src/lib/hooks/useDocumentDimensions.tsx
+++ packages/spfx-core/src/lib/hooks/useDocumentDimensions.tsx
@@ -1,30 +1,32 @@
 /* eslint-disable */
-import { useEffect, useState } from "react";
-import { AppContext } from "../app";
+import { useEffect, useState } from 'react';
+import { AppContext } from '../app';
 
 const currentContext = AppContext.getInstance();
 function getWindowDimensions(): DOMRect {
   const { domElement = document.body } = currentContext;
   const domRect = domElement?.getBoundingClientRect();
-  if (domRect) return domRect
+  if (domRect) return domRect;
 
   return {} as DOMRect;
 }
 
 export function useDocumentDimensions(): DOMRect {
   const { domRect } = currentContext;
-  const [windowDimensions, setWindowDimensions] = useState<DOMRect | null>(domRect);
+  const [windowDimensions, setWindowDimensions] = useState<DOMRect | null>(
+    domRect
+  );
 
-  useEffect(() => { 
-    function handleResize() { 
+  useEffect(() => {
+    function handleResize() {
       setWindowDimensions(() => getWindowDimensions());
     }
 
     handleResize(); // <-- invoke this on component mount
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
 
     return () => {
-      window.removeEventListener("resize", handleResize); 
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
@@ -33,4 +35,41 @@ export function useDocumentDimensions(): DOMRect {
   }
 
   return windowDimensions;
+}
+
+export function useGetElementDimensions(element: HTMLElement | null): DOMRect {
+  const [elementDimensions, setElementDimensions] = useState<DOMRect | null>(
+    element
+      ? element.getBoundingClientRect()
+      : ({
+          x: 0,
+          y: 0,
+          width: 0,
+          height: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+        } as DOMRect)
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setElementDimensions(() =>
+        element ? element.getBoundingClientRect() : ({} as DOMRect)
+      );
+    }
+
+    handleResize(); // <-- invoke this on component mount
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  if (!elementDimensions) {
+    return {} as DOMRect;
+  }
+
+  return elementDimensions;
 }
--- packages/spfx-core/src/lib/hooks/useGetTableHeight.tsx
+++ packages/spfx-core/src/lib/hooks/useGetTableHeight.tsx
@@ -1,9 +1,12 @@
-import { useDocumentDimensions } from "./useDocumentDimensions";
+import {
+  useDocumentDimensions,
+  useGetElementDimensions,
+} from './useDocumentDimensions';
 
 const DefaultHightOffset = 160;
 const DefaultMinHeight = 100;
 const DefaultMaxHeight = 9999;
-const DefaultHeightUnit = "px";
+const DefaultHeightUnit = 'px';
 export function useGetTableHeight(
   heightOffset: number = DefaultHightOffset,
   minHeight: number = DefaultMinHeight,
@@ -13,4 +16,16 @@ export function useGetTableHeight(
   const { height = 650 + heightOffset } = useDocumentDimensions();
   const tableHeight = Math.min(height - heightOffset, maxHeight);
   return `${Math.max(tableHeight, minHeight)}${unit}`;
+}
+
+export function useGetDocumentHeight(
+  element: HTMLElement | null,
+  heightOffset: number = DefaultHightOffset,
+  minHeight: number = DefaultMinHeight,
+  maxHeight: number = DefaultMaxHeight,
+  unit: string = DefaultHeightUnit
+): string {
+  const { height = 650 + heightOffset } = useGetElementDimensions(element);
+  const documentHeight = Math.min(height - heightOffset, maxHeight);
+  return `${Math.max(documentHeight, minHeight)}${unit}`;
 }
