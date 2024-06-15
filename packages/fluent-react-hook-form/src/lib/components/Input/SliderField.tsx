29af903386268fff49179443549657fa4155d09e
Pradeep Thapaliya <pradeep.thapaliya95@gmail.com>
2024-06-15 16:51:36 -0400

    sidebar

--- packages/fluent-react-hook-form/src/lib/components/Input/SliderField.tsx
+++ packages/fluent-react-hook-form/src/lib/components/Input/SliderField.tsx
@@ -1,61 +1,84 @@
-import { Field, FieldProps, Slider, SliderOnChangeData, SliderProps, LabelProps, InfoLabel, InfoLabelProps } from "@fluentui/react-components";
-import { forwardRef } from "react";
-import { useFormContext } from "../Form";
-import { Controller, ControllerProps } from "react-hook-form";
+import {
+  Field,
+  FieldProps,
+  Slider,
+  SliderOnChangeData,
+  SliderProps,
+  LabelProps,
+  InfoLabel,
+  InfoLabelProps,
+} from '@fluentui/react-components';
+import { forwardRef } from 'react';
+import { useFormContext } from '../Form';
+import { Controller, ControllerProps } from 'react-hook-form';
 
-export type SliderFieldProps = FieldProps & SliderProps & InfoLabelProps & {
-    name: string,
-    rules?: ControllerProps['rules']
-}
+export type SliderFieldProps = FieldProps &
+  SliderProps &
+  InfoLabelProps & {
+    name: string;
+    rules?: ControllerProps['rules'];
+  };
 
-export const SliderField = forwardRef<HTMLInputElement, SliderFieldProps>(({ name, rules, required, ...rest }, SliderRef) => {
-    const { form: { control } } = useFormContext();
+export const SliderField = forwardRef<HTMLInputElement, SliderFieldProps>(
+  ({ name, rules, required, ...rest }, SliderRef) => {
+    const {
+      form: { control },
+    } = useFormContext();
 
     const { ...fieldProps }: FieldProps = rest;
     const { ...sliderProps }: SliderProps = rest;
     const { ...infoLabelProps }: InfoLabelProps = rest;
 
     return (
-        <Controller
-            name={name}
-            control={control}
-            rules={rules}
-            render={({ field, fieldState }) => {
-                const { onChange, onBlur, value, ref } = field;
+      <Controller
+        name={name}
+        control={control}
+        rules={rules}
+        render={({ field, fieldState }) => {
+          const { onChange, onBlur, value, ref } = field;
 
-                const handleOnChange : SliderProps["onChange"] = (ev: React.ChangeEvent<HTMLInputElement>, data: SliderOnChangeData) => {
-                    onChange(data.value);
-                    sliderProps.onChange?.(ev, data);
-                }
+          const handleOnChange: SliderProps['onChange'] = (
+            ev: React.ChangeEvent<HTMLInputElement>,
+            data: SliderOnChangeData
+          ) => {
+            onChange(data.value);
+            sliderProps.onChange?.(ev, data);
+          };
 
-                const handleOnBlur : SliderProps["onBlur"]  = (ev: React.FocusEvent<HTMLInputElement>) => {
-                    onBlur();
-                    sliderProps.onBlur?.(ev);
-                }
+          const handleOnBlur: SliderProps['onBlur'] = (
+            ev: React.FocusEvent<HTMLInputElement>
+          ) => {
+            onBlur();
+            sliderProps.onBlur?.(ev);
+          };
 
-                return (
-                    <Field
-                        {...fieldProps}
-                        label={{
-                            children: (_: unknown, props: LabelProps) => (
-                                <InfoLabel {...props} {...infoLabelProps} />
-                            )
-                        } as unknown as InfoLabelProps}
-                        validationState={fieldState.invalid ? "error" : undefined}
-                        validationMessage={fieldState.error?.message}
-                        required={required}
-                    >
-                        <Slider
-                            {...sliderProps}
-                            ref={SliderRef || ref}
-                            name={name}
-                            onChange={handleOnChange}
-                            onBlur={handleOnBlur}
-                            value={value || 0}
-                            required={false}
-                        />
-                    </Field>
-                )
-            }}
-        />)
-})
+          return (
+            <Field
+              {...fieldProps}
+              label={
+                {
+                  children: (_: unknown, props: LabelProps) => (
+                    <InfoLabel {...props} {...infoLabelProps} />
+                  ),
+                } as unknown as InfoLabelProps
+              }
+              validationState={fieldState.invalid ? 'error' : undefined}
+              validationMessage={fieldState.error?.message}
+              required={required}
+            >
+              <Slider
+                {...sliderProps}
+                ref={SliderRef || ref}
+                name={name}
+                onChange={handleOnChange}
+                onBlur={handleOnBlur}
+                value={+(value || 0)}
+                required={false}
+              />
+            </Field>
+          );
+        }}
+      />
+    );
+  }
+);
